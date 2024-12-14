3833f96a613a6b4d62fe61c3658a96d4220ebb26
Eleeeeeynnn <0322-1409@lspu.edu.ph>
2024-12-14 09:33:17 +0800

    Add files via upload

---
+++ BalatbatGUI/src/balatbatgui/BalatbatGUI.java
@@ -0,0 +1,14 @@
+
+package balatbatgui;
+
+public class BalatbatGUI {
+
+   
+    public static void main(String[] args) 
+    {
+        FrmTicTacToeBalatbat tiktak = new FrmTicTacToeBalatbat();
+        tiktak.setVisible(true);
+    }
+    
+}
+
